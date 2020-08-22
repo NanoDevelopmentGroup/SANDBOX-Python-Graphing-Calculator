aba6fb9a4747df9a14310bdc6f693f52c755832e
Logan Tellier <logan.tellier@outlook.com>
2020-08-22 01:05:05 -0400

    feat: reconfigure statusbar logger to be scrolled
    Added a new signal for thread-safety to handle the scrolling of the plaintextedit as logs are appended. Also set the scrollbars to active so the user may review previous logs and otherwise navigate.

--- autograph/common/status_logger.py
+++ autograph/common/status_logger.py
@@ -10,25 +10,28 @@ from PyQt5 import QtCore, QtWidgets
 
 class QTextEditLogger(logging.Handler, QtCore.QObject):
 
-    setPlainText = QtCore.pyqtSignal(str)
+    appendPlainText = QtCore.pyqtSignal(str)
+    setScrollValue = QtCore.pyqtSignal(int)
 
     def __init__(self, status_bar: QtWidgets.QStatusBar):
         super().__init__()
         QtCore.QObject.__init__(self)
         self.widget = QtWidgets.QPlainTextEdit(status_bar)
         self.widget.setReadOnly(True)
-        self.setPlainText.connect(self.widget.setPlainText)
+        self.setScrollValue.connect(self.widget.verticalScrollBar().setValue)
+        self.appendPlainText.connect(self.widget.appendPlainText)
 
         status_bar.addPermanentWidget(self.widget,1)
         status_bar.setContentsMargins(8,2,0,4)
 
         self.widget.setFrameShape(QtWidgets.QFrame.NoFrame)
         self.widget.setFrameShadow(QtWidgets.QFrame.Plain)
-        self.widget.setMaximumHeight(20)
-        self.widget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.widget.setMaximumHeight(18)
+        self.widget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
         self.widget.setPlaceholderText("Initializing Logger ...")
 
     def emit(self, record):
         msg = self.format(record)
-        self.setPlainText.emit(msg)
+        self.appendPlainText.emit(msg)
+        self.setScrollValue.emit(self.widget.verticalScrollBar().maximum() - 1)
 
