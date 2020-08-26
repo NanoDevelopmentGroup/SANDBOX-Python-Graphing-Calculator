ba3ad658888385d62b2a28f0d5c9eb550a1b01b5
Logan Tellier <logan.tellier@outlook.com>
2020-08-25 20:56:01 -0400

    chore: Document the QTextEditLogger class
    Incorporates extended comments and docstrings for this class.

--- autograph/common/status_logger.py
+++ autograph/common/status_logger.py
@@ -19,10 +19,8 @@ class QTextEditLogger(logging.Handler, QtCore.QObject):
     and signals are not hardcoded, but rather the UI formatting.
 
     Inherits:
-        logging.Handler: for functionality to handle logs as created
-        across the higher level application.
-        QtCore.QObject: for general Qt functionality, as well as to
-        use pyqtSignals for threadsafe operations.
+        logging.Handler: for functionality to handle logs as created across the higher level application.
+        QtCore.QObject: for general Qt functionality, as well as to use pyqtSignals for threadsafe operations.
     """
 
     # Create a signal to manage the appending of a single line of text
@@ -32,20 +30,36 @@ class QTextEditLogger(logging.Handler, QtCore.QObject):
     setScrollValue = QtCore.pyqtSignal(int)
 
     def __init__(self, status_bar: QtWidgets.QStatusBar):
+        """Initializes a QTextEditLogger within a specified QStatusBar parent.
+
+        Args:
+            status_bar (QtWidgets.QStatusBar): the parent of the QTextEditLogger, must be of type QStatusBar.
+            This requirement is due to UI formatting and could be lifted with generalized or dynamic formatting.
+        """
         super().__init__()
         QtCore.QObject.__init__(self)
+
+        # Instantiate the QPlainTextEdit widget with Qt functionality
         self.widget = QtWidgets.QPlainTextEdit(status_bar)
+
+        # Set this widget to be ReadOnly to facilitate logging display
         self.widget.setReadOnly(True)
+
+        # Connect the defined pyqt signals to their respective functionality
         self.setScrollValue.connect(self.widget.verticalScrollBar().setValue)
         self.appendPlainText.connect(self.widget.appendPlainText)
 
+        # Add the widget as the first in the statusbar, set static formatting
         status_bar.addPermanentWidget(self.widget,1)
         status_bar.setContentsMargins(8,2,0,4)
-
         self.widget.setFrameShape(QtWidgets.QFrame.NoFrame)
         self.widget.setFrameShadow(QtWidgets.QFrame.Plain)
         self.widget.setMaximumHeight(18)
+
+        # Turn scrollbar on to interact with the log viewport manually
         self.widget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+
+        # Generate placeholder text which will be replaced by the first log message
         self.widget.setPlaceholderText("Initializing Logger ...")
 
     def emit(self, record):
